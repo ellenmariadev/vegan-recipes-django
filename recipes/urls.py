2ce0b18bcbc9dfbf5781828c7b0d91bb881d7dd4
ellenmariadev <ellenmariadev@gmail.com>
2022-11-20 01:01:49 -0300

    feat: model + home and details views

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('recipes/category/<int:id>/', views.recipe_category, name='categories'),
     path('recipe/<int:id>/', views.recipe, name='recipe')
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+
+from .models import Recipe
 
 # Create your views here.
 
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
         'title': 'Receitas',
-        'cssfile': 'home.css'
+        'recipes': recipes
     })
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe.objects.filter(
+        pk=id, is_published=True).order_by('-id'))
+
     return render(request, 'recipes/pages/recipe-detail.html', context={
-        'title': 'Receitas'
+        'title': '',
+        'recipe': recipe
+    })
+
+
+def recipe_category(request, id):
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=id, is_published=True).order_by('-id'))
+
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes
     })
