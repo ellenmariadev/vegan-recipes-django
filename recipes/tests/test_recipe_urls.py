ce966677f2b52910165a0871fd5dbbc82d68e6c0
ellenmariadev <ellenmariadev@gmail.com>
2022-11-20 18:59:06 -0300

    recipes tests

---
+++ recipes/tests/test_recipe_urls.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.urls import reverse
+
+# Create your tests here.
+
+
+class RecipeURLsTest(TestCase):
+    def test_recipe_home_url_is_correct(self):
+        url = reverse('home')
+        self.assertEqual(url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('categories', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_detail_url_is_correct(self):
+        url = reverse('recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipe/1/')
--- recipes/urls.py
+++ recipes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/category/<int:id>/', views.recipe_category, name='categories'),
+    path('', views.home, name='home'),
+    path('recipes/category/<int:id>/', views.recipe_category, name='categories'),  # noqa E501
     path('recipe/<int:id>/', views.recipe, name='recipe')
 ]
